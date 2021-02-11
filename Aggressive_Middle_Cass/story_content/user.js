a6b3b40f2cbba43db230bc21a3ad2b75fc21133d
Karan <karant.packtpub@gmail.com>
2021-02-11 18:23:27 +0530

    Add files via upload

---
+++ Aggressive_Middle_Cass/story_content/user.js
@@ -0,0 +1,103 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ZATRBp9tqU":
+        Script1();
+        break;
+      case "66erHweiOMW":
+        Script2();
+        break;
+      case "6TBjpP600w5":
+        Script3();
+        break;
+      case "6cG8O3RxBzj":
+        Script4();
+        break;
+      case "6e58Bsgg1uJ":
+        Script5();
+        break;
+      case "5Y4TDiLE7kN":
+        Script6();
+        break;
+      case "6g0bGqXKcGM":
+        Script7();
+        break;
+      case "5WS2UDcpmqv":
+        Script8();
+        break;
+      case "6hCxerPS6xX":
+        Script9();
+        break;
+      case "6Ry3wfHKLPH":
+        Script10();
+        break;
+      case "6k5NdbVKkzT":
+        Script11();
+        break;
+      case "5iuoP2sXmbv":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script2()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script3()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script4()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script5()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script6()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script7()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script8()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script9()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script10()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script11()
+{
+  window.postMessage("slideLoaded")
+}
+
+function Script12()
+{
+  window.postMessage("slideLoaded")
+}
+
